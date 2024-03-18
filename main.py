7f5268ae7f2e244af8304113c0c1ce10b7484bbb
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-03-17 19:29:41 -0500

    creating the tool structure and adding prompt

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from dotenv import load_dotenv
-
+from langchain.agents import tool
 load_dotenv()
 
 
-def get_text_length(text: str)-> int:
+
+@tool
+def get_text_length(text: str) -> int:
     """Return the length of a text by characters"""
     return len(text)
+
+
 if __name__ == "__main__":
     print("Hello react")
-    print(get_text_length(text="dog"))
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:{agent_scratchpad}
+    """
